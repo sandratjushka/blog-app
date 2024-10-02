6948bb36fc44dfe9c31914f38b82f07462ef8479
Sandra Tjushka <sandratjushka@gmail.com>
2024-10-02 13:26:09 +0300

    posts and comment services creation

---
+++ posts/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+console.log('Starting the server...'); // Debugging line
+
+const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    console.log('GET /posts endpoint called'); // Debugging line
+    res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+    console.log('POST /posts endpoint called'); // Debugging line
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+
+    posts[id] = {
+        id, title
+    };
+
+    res.status(201).send(posts[id]);
+});
+
+app.listen(3000, () => {
+    console.log('Listening on 3000'); // Debugging line
+});
